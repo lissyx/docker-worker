1f4bf4f9e091b335e05cbc557fcaf1d81373eab1
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-04-10 15:07:33 +0200

    Hacking some generic-linux host

--- src/bin/worker.js
+++ src/bin/worker.js
@@ -27,7 +27,7 @@ const typedEnvConfig = require('typed-env-config');
 const SchemaSet = require('taskcluster-lib-validate');
 
 // Available target configurations.
-var allowedHosts = ['aws', 'test', 'packet', 'taskcluster-worker-runner'];
+var allowedHosts = ['aws', 'test', 'packet', 'taskcluster-worker-runner', 'generic-linux'];
 let debug = Debug('docker-worker:bin:worker');
 
 // All overridable configuration options from the CLI.
